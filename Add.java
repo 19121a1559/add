cb7ac01ae8a52e498872031390c2a8bd2272b721
19121a1559 <kopparaputejasree@gmail.com>
2024-03-01 12:13:48 +0530

    first commit

---
+++ Add.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Add{
+    public static int add(int a, int b){
+        return a+b;
+    }
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int x=scanner.nextInt();
+        int y=scanner.nxtInt();
+        add(a,b);
+
+
+    }
+}
